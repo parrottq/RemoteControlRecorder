1df8b0b8dcfc1c6660d1754f95cb38d8dd7afdbb
quinn <quinn.parrott@gmail.com>
2016-02-02 12:31:16 -0600

    Made Buffer thread safe

--- src/com/parrott/Buffer.java
+++ src/com/parrott/Buffer.java
@@ -7,26 +7,31 @@ public class Buffer {
 
     private ByteBuffer buffer;
     private boolean isWritable = true;
+    private Object lock;
 
     public Buffer() {
         this.buffer = ByteBuffer.allocate(1024);
     }
 
-    private void write(byte write) {
-        if (isWritable) {
-            buffer.put(write);
-        } else {
-            flip();
-            write(write);
+    public void write(byte write) {
+        synchronized (lock) {
+            if (isWritable) {
+                buffer.put(write);
+            } else {
+                flip();
+                write(write);
+            }
         }
     }
 
-    private byte read() {
-        if (isWritable) {
-            flip();
-            return read();
-        } else {
-            return buffer.get();
+    public byte read() {
+        synchronized (lock) {
+            if (isWritable) {
+                flip();
+                return read();
+            } else {
+                return buffer.get();
+            }
         }
     }
 
