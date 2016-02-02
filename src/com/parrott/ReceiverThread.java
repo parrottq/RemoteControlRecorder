88416ba86a8e17ca32d42881ab0ea8965df91bee
Quinn <quinn.parrott@gmail.com>
2016-02-01 20:19:45 -0600

    Create socket

--- src/com/parrott/ReceiverThread.java
+++ src/com/parrott/ReceiverThread.java
@@ -1,9 +1,33 @@
 package com.parrott;
 
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ReceiverThread implements Runnable {
+
+    private Socket socket;
+    private DataOutputStream outputStream;
+
+    public String address;
+    public boolean done = false;
+
+    public ReceiverThread(String address){
+        this.address = address;
+    }
+
     @Override
     public void run() {
+        try {
+            socket = new Socket(address, 1234);
+            outputStream = new DataOutputStream(socket.getOutputStream());
+
+            while (!done){
+                break;
+            }
+
+        } catch (IOException e) {e.printStackTrace();}
 
     }
 }
