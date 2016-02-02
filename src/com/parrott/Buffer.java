eab823bb6818f3219c961cde91e2c37c8ed21688
Quinn <quinn.parrott@gmail.com>
2016-02-01 21:08:26 -0600

    Populated Buffer class

--- src/com/parrott/Buffer.java
+++ src/com/parrott/Buffer.java
@@ -1,5 +1,37 @@
 package com.parrott;
 
 
+import java.nio.ByteBuffer;
+
 public class Buffer {
+
+    private ByteBuffer buffer;
+    private boolean isWritable = true;
+
+    public Buffer() {
+        this.buffer = ByteBuffer.allocate(1024);
+    }
+
+    private void write(byte write) {
+        if (isWritable) {
+            buffer.put(write);
+        } else {
+            flip();
+            write(write);
+        }
+    }
+
+    private byte read() {
+        if (isWritable) {
+            flip();
+            return read();
+        } else {
+            return buffer.get();
+        }
+    }
+
+    private void flip() {
+        buffer.flip();
+        isWritable = !isWritable;
+    }
 }
