62c7ee9006e8d691a094d933d030aaf2f9e749d8
Quinn <quinn.parrott@gmail.com>
2016-02-03 20:32:13 -0600

    Program writes to file

--- src/com/parrott/ReceiverThread.java
+++ src/com/parrott/ReceiverThread.java
@@ -2,33 +2,35 @@ package com.parrott;
 
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ReceiverThread extends Thread {
 
-    private Socket socketOut;
-    private DataOutputStream outputStream;
+    //private Socket socketOut;
+    //private DataOutputStream outputStream;
 
+    public String address;
+    public String path;
+    public boolean done = false;
     private ServerSocket serverSocket;
     private Socket socketIn;
     private DataInputStream inputStream;
+    private FileWriter file;
 
-    public String address;
-    public boolean done = false;
-
-    public ReceiverThread(String address){
+    public ReceiverThread(String address, String path) {
         this.address = address;
+        this.path = path;
     }
 
     @Override
     public void run() {
         System.out.println("Started");
         try {
-            socketOut = new Socket(address, 1234);
-            outputStream = new DataOutputStream(socketOut.getOutputStream());
+            //socketOut = new Socket(address, 1234);
+            //outputStream = new DataOutputStream(socketOut.getOutputStream());
             System.out.println("Connected");
 
             serverSocket = new ServerSocket(4321);
@@ -36,9 +38,14 @@ public class ReceiverThread extends Thread {
             inputStream = new DataInputStream(socketIn.getInputStream());
             System.out.println("Receiving");
 
+            file = new FileWriter(path);
+
             while (!done){
+                System.out.println(")8(");
                 int read = inputStream.read();
-                outputStream.write(read);
+                //outputStream.write(read);
+                file.write(read);
+                file.flush();
             }
 
         } catch (IOException e) {e.printStackTrace();}
