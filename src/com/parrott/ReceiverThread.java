44c1e8c6dc515d40200736da33ee3793a78acfb1
Quinn <quinn.parrott@gmail.com>
2016-02-03 21:34:47 -0600

    Added all the recording logic

--- src/com/parrott/ReceiverThread.java
+++ src/com/parrott/ReceiverThread.java
@@ -40,12 +40,53 @@ public class ReceiverThread extends Thread {
 
             file = new FileWriter(path);
 
+            System.out.println("Recording");
+            int[] actions = {0, 0};
+            int count = 0;
+
+            int right = 0;
+            int left = 0;
+            boolean[] changed = {false, false};
+
             while (!done){
-                System.out.println(")8(");
                 int read = inputStream.read();
                 //outputStream.write(read);
-                file.write(read);
-                file.flush();
+                if (read == 0x00) {
+                    count = 0;
+                    actions[0] = 0;
+                    actions[1] = 0;
+                } else {
+                    if (count == 1) {
+                        actions[0] = read;
+                    } else if (count == 2) {
+                        actions[1] = read;
+                    }
+                }
+
+                if (count == 2) {
+                    System.out.print(actions[0] + "-" + actions[1] + "\n");
+                    if (actions[0] == 0x31) {
+                        System.out.println("Right");
+                        right = actions[1];
+                        changed[0] = true;
+                    } else if (actions[0] == 0x32) {
+                        System.out.println("Left");
+                        left = actions[1];
+                        changed[1] = true;
+                    }
+                    System.out.println(changed[0] + "+" + changed[1]);
+
+                    if (changed[0] && changed[1]) {
+                        System.out.println("Pushed");
+                        changed[0] = false;
+                        changed[1] = false;
+                        System.out.println(right);
+
+                        file.write(String.valueOf(right) + ':' + String.valueOf(left) + ":50:");
+                        file.flush();
+                    }
+                }
+                count++;
             }
 
         } catch (IOException e) {e.printStackTrace();}
